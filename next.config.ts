aa54c45d06bb0aad702b4234cf8256008f3ab248
aaron hu <flyhsyy@gmail.com>
2026-01-23 11:29:42 +0800

    [chore] change to static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // 1. 开启静态导出：生成纯 HTML/CSS/JS 到 out 文件夹
+    output: 'export',
+
+    // 2. 关闭图片优化：因为没有 Node.js 服务器处理图片压缩
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
